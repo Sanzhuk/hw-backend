1959b1ef7a224b3036d202aa173d51689216a767
Sanzhar Turganbek <tsancho23@gmail.com>
2025-01-28 00:14:32 +0900

    solution

--- cars/app/main.py
+++ cars/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Query, HTTPException
+from typing import List, Annotated
 
 from .cars import create_cars
 
@@ -10,8 +11,26 @@ app = FastAPI()
 def index():
     return Response("<a href='/cars'>Cars</a>")
 
-
 # (сюда писать решение)
 
+@app.get("/cars", response_model=List[dict])
+def get_cars(
+    page: int = Query(default=1, ge=1, le=100, description="The page number"),
+    limit: int = Query(default=10, ge=1, le=100, description="The number of elements per page")
+):
+    start_idx = (page - 1) * limit
+    end_idx = min(start_idx + limit, 100)
+    
+    return cars[start_idx:end_idx]
+
+@app.get("/cars/{id}")
+def get_cars_id(
+    id: int,  
+):
+    for car in cars:
+        if car["id"] == id:
+            return car
+        
+    raise HTTPException(status_code=404, detail="Not found")
 
 # (конец решения)
--- users/app/main.py
+++ users/app/main.py
@@ -1,21 +1,31 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-from .users import create_users
+from users import create_users
 
 users = create_users(100)  # Здесь хранятся список пользователей
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="../templates")
 
 
 @app.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-
 # (сюда писать решение)
 
+@app.get("/users", response_class=HTMLResponse)
+def get_all_users(request: Request):
+    return templates.TemplateResponse("users/index.html", {'request':request, 'result':users, 'result_len':len(users)})
 
+@app.get("/users/{id}", name="user_detail")
+def user_detail(request: Request, id: int):
+    for user in users:
+        if user['id'] == id:
+            target_user = user
+    
+    return templates.TemplateResponse("users/user.html", {'request':request, 'result':target_user})
 
 # (конец решения)
